6c29bcc791028b3a40472ab633afc8c3f1157bc5
sanket240494 <sanketladke007@gmail.com>
2023-01-15 14:54:28 +0530

    JSON_SERVER-API_AUTOMATION

---
+++ src/main/java/CQA_112_JSON_API_AUTOMATION/CQA_112_JSON_API_AUTOMATION/App.java
@@ -0,0 +1,13 @@
+package CQA_112_JSON_API_AUTOMATION.CQA_112_JSON_API_AUTOMATION;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
